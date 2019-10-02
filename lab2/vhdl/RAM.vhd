6efeb152d849a55779b359eedf6be0e3af5c6511
tdemont <tom.demont@epfl.ch>
2019-10-02 11:39:51 +0200

    Corrects RAM

--- lab2/vhdl/RAM.vhd
+++ lab2/vhdl/RAM.vhd
@@ -18,12 +18,7 @@ architecture synth of RAM is
 	signal ram         : ram_type;
 	signal address_FF  : std_logic_vector(9 downto 0);
 	signal cs_FF       : std_logic;
-	signal rd_selected : std_logic_vector(31 downto 0);
 begin
-
-	rddata <= rd_selected when cs_FF = '1' else (others => 'Z');
-
-	rd_selected <= ram(to_integer(unsigned(address_FF)));
 
 	cs_address : process(clk) is
 	begin
@@ -33,12 +28,11 @@ begin
 		end if;
 	end process;
 
-	read_sel : process(cs_FF, rd_selected) is
+	read_sel : process(cs_FF, address_FF, ram) is
 	begin
+		rddata <= (others => 'Z');
 		if cs_FF = '1' then
-			rddata <= rd_selected;
-		else
-			rddata <= (others => 'Z');
+			rddata <= ram(to_integer(unsigned(address_FF)));	
 		end if;
 	end process;
 
