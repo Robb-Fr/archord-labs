b92ac702814863856c0714679c5a967946e12665
tdemont <tom.demont@epfl.ch>
2019-10-09 11:27:12 +0200

    Implements elements of the first CPU but controller

--- lab3/vhdl/PC.vhd
+++ lab3/vhdl/PC.vhd
@@ -17,5 +17,24 @@ entity PC is
 end PC;
 
 architecture synth of PC is
+	signal cur, nxt : std_logic_vector(31 downto 0);
 begin
+	addr <= cur(31 downto 2)&"00";
+	
+	dff : process (clk, reset_n) is 
+	begin
+		if reset_n = '0' then
+			cur <= (others => '0');
+		elsif rising_edge(clk) then
+			cur <= nxt;
+		end if;
+	end process;
+	
+	next_state : process (cur, en) is
+	begin
+		if en = '1' then
+			nxt <= std_logic_vector((unsigned(cur)+4));
+		end if;
+	end process;
+	
 end synth;
