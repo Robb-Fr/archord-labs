cc88029357014ddd1e20f9ee3f21c7b1ee980377
tdemont <tom.demont@epfl.ch>
2019-10-19 16:31:31 +0200

    fou. malade.

--- lab3/vhdl/PC.vhd
+++ lab3/vhdl/PC.vhd
@@ -39,7 +39,7 @@ begin
 		elsif add_imm = '0' and sel_a = '0' and sel_imm = '1' then
 			nxt <= std_logic_vector(X"000" & "00" & (imm(15 downto 0) & "00"));
 		elsif add_imm = '0' and sel_a = '1' and sel_imm = '0' then
-			nxt <= std_logic_vector(X"000" & "00" & (a(15 downto 0) & "00"));
+			nxt <= std_logic_vector(X"0000" & (a(15 downto 0)));
 		else
 			nxt <= std_logic_vector((unsigned(cur) + 4));
 		end if;
