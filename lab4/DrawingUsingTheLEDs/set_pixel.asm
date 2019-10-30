3d710c0e8f536e7c50c9c91b50fb7d7ba36897fc
unknown <harold.benoit@epfl.ch>
2019-10-30 17:18:04 +0100

    Implement better set_pixel

--- lab4/DrawingUsingTheLEDs/set_pixel.asm
+++ lab4/DrawingUsingTheLEDs/set_pixel.asm
@@ -14,14 +14,8 @@ and t3, a0, t0    # t3 is the offset 0,1,2,3 to be used to find the correct bit 
 addi t4,zero,1    # t4 will be the mask for the correct bit
 sll  t4,t4, a1    # t4 has now been shifted by y and will be then shifted by offset*8
 
-beq t3,zero, if_zero
-
-if_not_zero :
-slli t4,t4,8                       #shifting by offset*8
-addi t3,t3,-1
-bne t3, zero, if_not_zero
-
-if_zero :
+slli t3,t3,3      # offset*8
+sll t4,t4,t3      # shfiting t4 by offset*8         
 or t2,t2,t4
 stw t2,LEDS(t1)
 ret
