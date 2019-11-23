eb515f4e24ac62ab4d0771db0a787275c3339f15
tdemont <tom.demont@epfl.ch>
2019-11-23 19:11:05 +0100

    wait

--- lab4/assembly/tetris.asm
+++ lab4/assembly/tetris.asm
@@ -174,7 +174,7 @@ set_pixel:
 ; BEGIN:wait
 wait:
 	addi a0, zero, 0x1
-	slli a0, a0, 0				# sets the 20th bit to 1 in order to have 2^20
+	slli a0, a0, 20				# sets the 20th bit to 1 in order to have 2^20
 
 	count_down:
 		addi a0, a0, -1 			# decrement argument by 1
